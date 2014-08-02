a418256c36388be0e4cb90a7de96999c4c771ebf
empeeu <empeeu@users.noreply.github.com>
2014-08-02 16:55:05 -0400

    Initial commit

---
+++ Mock_Lecture/mpl_centered_spines.py
@@ -0,0 +1,9 @@
+def center_spines(ax):
+    ax.spines['left'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['top'].set_color('none')
+    ax.spines['left'].set_smart_bounds(True)
+    ax.spines['bottom'].set_smart_bounds(True)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
